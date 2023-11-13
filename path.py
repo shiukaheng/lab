f5bc52dbb0258159f426bc0ac887b70093ce9dee
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-11-13 17:24:32 +0000

    3d rtt impl

--- path.py
+++ path.py
@@ -6,6 +6,7 @@ Created on Thu Sep 21 11:44:32 2023
 @author: stonneau
 """
 
+from typing import Optional
 import pinocchio as pin
 import numpy as np
 from numpy.linalg import pinv
@@ -14,7 +15,16 @@ from config import LEFT_HAND, RIGHT_HAND
 import time
 
 from rrt_star import *
-from inverse_geometry_utils import *
+
+class RTTStarNodeImpl(RTTNode):
+    def __init__(self, 
+                 point: np.ndarray, 
+                 left: Optional['RTTNode'] = None, 
+                 right: Optional['RTTNode'] = None,
+                 ):
+        super().__init__(point, left, right)
+        self.q = None
+
 
 class RTTStarImpl(RTTStar):
     # Make init pass all arguments to super
@@ -22,12 +32,12 @@ class RTTStarImpl(RTTStar):
                     robot,
                     cube,
                     viz,
-                    dimensions: int=13,
+                    dimensions: int=3,
                     initial: Optional[np.ndarray] = None,
-                    step_size: float = 0.1,
-                    neighbor_radius: float = 0.3,
-                    lower_bounds: Optional[np.ndarray] = None,
-                    upper_bounds: Optional[np.ndarray] = None,
+                    step_size: float = 0.05,
+                    neighbor_radius: float = 0.2,
+                    lower_bounds: Optional[np.ndarray] = np.array([0.4, -0.4, 0.93]),
+                    upper_bounds: Optional[np.ndarray] = np.array([0.5, 0.4, 1.5]),
                     collision_samples: int = 10,
                     goal: Optional[np.ndarray] = None,
                     goal_radius: float = 0.05,
@@ -38,14 +48,8 @@ class RTTStarImpl(RTTStar):
             self.cube = cube
             self.viz = viz
     def check_point_collision(self, point: np.ndarray) -> bool:
-        q = to_full(point)
-        pin.framesForwardKinematics(self.robot.model, self.robot.data, q)
-        pin.updateGeometryPlacements(robot.model,robot.data,robot.collision_model,robot.collision_data,q)
-        return pin.computeCollisions(robot.collision_model,robot.collision_data,False)
-        # Attempt inverse geometry to see if we can get collision free config
-        # Optimization: Use the last point as a starting point
-        # Optimization 2: Instantly terminate if we are in collision
-        # Issue: Given we are incrementally optimizing from the last pose, how do we guarantee we can smoothly transition to the goal pose?
+        neighbour = self.nearest_neighbor(point)
+        pass
 
 
 #returns a collision free path from qinit to qgoal under grasping constraints
@@ -53,10 +57,8 @@ class RTTStarImpl(RTTStar):
 def computepath(qinit,qgoal,cubeplacementq0, cubeplacementqgoal, robot, cube, viz):
     path = RTTStarImpl(
         robot, cube, viz,
-        initial=to_compact(qinit),
-        goal=to_compact(qgoal),
-        lower_bounds=to_compact(robot.model.lowerPositionLimit),
-        upper_bounds=to_compact(robot.model.upperPositionLimit),
+        initial=cubeplacementq0.translation,
+        goal=cubeplacementqgoal.translation
     ).solve()
     return path
 
@@ -82,7 +84,6 @@ if __name__ == "__main__":
         print ("error: invalid initial or end configuration")
     
     path = computepath(q0,qe,CUBE_PLACEMENT, CUBE_PLACEMENT_TARGET, robot, cube, viz)
-    path = [to_full(q) for q in path]
     
-    displaypath(robot,path,dt=0.1,viz=viz) #you ll probably want to lower dt
+    # displaypath(robot,path,dt=0.5,viz=viz) #you ll probably want to lower dt
     
