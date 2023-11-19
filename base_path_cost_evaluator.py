5bff91581ab99486741f918343f735f2185da835
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-11-19 00:11:52 +0000

    more tests

--- base_path_cost_evaluator.py
+++ base_path_cost_evaluator.py
@@ -1,4 +1,6 @@
 import numpy as np
+from path_cost_utils import *
+from scipy.interpolate import interp1d
 
 def path_cost(weight):
     def decorator(func):
@@ -35,6 +37,12 @@ def pose_cost(weight):
         return func
     return decorator
 
+def access_dict_safe(dictionary, key):
+    if key in dictionary:
+        return dictionary[key]
+    else:
+        return None
+
 class PathCostMeta(type):
     def __new__(cls, name, bases, dct):
         path_cost_functions = []
@@ -65,29 +73,55 @@ class BasePathCostEvaluator(metaclass=PathCostMeta):
         self.robot = robot
         self.cube = cube
 
-    def compute(self, path):
+    def _compute_cost(self, path, frame_costs_cache=None, frame_indices=None):
+        consumed_path = [tuple(frame) for frame in path]
+        if frame_costs_cache is None:
+            frame_costs_cache = {}
         total_cost = 0
         for func, weight in self.path_cost_functions:
-            total_cost += func(self, path) * weight
-        cube_path = self.extract_cube_path(path)
+            total_cost += func(self, consumed_path) * weight
+        cube_path = np.array(list(self.extract_cube_path(path)))
         for func, weight in self.cube_path_cost_functions:
             total_cost += func(self, cube_path) * weight
-        pose_path = self.extract_pose_path(path)
+        pose_path = np.array(list(self.extract_pose_path(path)))
         for func, weight in self.pose_path_cost_functions:
             total_cost += func(self, pose_path) * weight
-        frame_costs = 0
-        for (cube, pose) in path:
-            for func, weight in self.cube_cost_functions:
-                frame_costs += func(self, cube) * weight
-            for func, weight in self.pose_cost_functions:
-                frame_costs += func(self, pose) * weight
-        total_cost += frame_costs / len(path)
-        return total_cost
+        total_frame_costs = 0
+        if frame_indices is None:
+            frame_indices = range(len(path))
+        for ((cube, pose), index) in zip(path, frame_indices):
+            cached = access_dict_safe(frame_costs_cache, index)
+            if cached is not None:
+                total_frame_costs += cached
+            else:
+                frame_cost = 0
+                for func, weight in self.cube_cost_functions:
+                    frame_cost += func(self, cube) * weight
+                for func, weight in self.pose_cost_functions:
+                    frame_cost += func(self, pose) * weight
+                total_frame_costs += frame_cost
+                frame_costs_cache[index] = frame_cost
+        total_cost += total_frame_costs / len(path)
+        return total_cost, frame_costs_cache
     
-    @path_cost(weight=1)
-    def cube_path_length(self, path):
-        cube_positions = self.extract_cube_path(path)
-        return np.linalg.norm(np.diff(cube_positions, axis=0), axis=1).sum()
+    def compute_cost(self, path):
+        return self._compute_cost(np.array(path, dtype=object))[0]
+    
+    def compute_local_cost(self, path, window_size=5, stride=1):
+        # Get list of indices
+        window_centers, local_costs = self.compute_local_costs_non_interpolated(path, window_size, stride)
+        interp_func = interp1d(window_centers, local_costs, kind='linear', fill_value='extrapolate')
+        return interp_func(np.arange(len(path)))
+
+    def compute_local_costs_non_interpolated(self, path, window_size, stride):
+        windows, window_centers = get_windows(window_size, stride, len(path))
+        frame_cost_cache = {}
+        local_costs = []
+        path_numpy = np.array(path, dtype=object)
+        for window in windows:
+            cost, frame_cost_cache = self._compute_cost(path_numpy[window], frame_cost_cache, window)
+            local_costs.append(cost)
+        return window_centers,local_costs
 
     def extract_cube_path(self, path):
         return np.array(path)[:, 0]
--- path_cost_utils.py
+++ path_cost_utils.py
@@ -9,3 +9,26 @@ def smoothness(array):
     costs = (dot_products - 1) / -2
     smoothness = np.mean(costs ** 2)
     return smoothness
+
+def get_window_centers(window_size, stride, length_of_path):
+    if length_of_path < window_size:
+        raise ValueError("Path length must be greater than window size")
+    all_indices = np.arange(length_of_path) # [0, 1, 2, ..., length_of_path - 1]
+    legal_indices = all_indices[window_size//2:-window_size//2] # [window_size//2, ..., length_of_path - 1 - window_size//2]
+    sampled_indices = legal_indices[::stride] # [window_size//2, window_size//2 + stride, ..., length_of_path - 1 - window_size//2]
+    return sampled_indices
+
+def get_windows(window_size, stride, length_of_path):
+    sampled_indices = get_window_centers(window_size, stride, length_of_path)
+    windows = []
+    for index in sampled_indices:
+        windows.append(np.arange(index - window_size//2, index + window_size//2 + 1))
+    return np.array(windows, dtype=int), sampled_indices
+
+def calc_local_cost(path, window_size, stride, cost_function):
+    windows, window_centers = get_windows(window_size, stride, len(path))
+    local_costs = []
+    for window in windows:
+        local_costs.append(cost_function(path[window]))
+    interp_func = interp1d(window_centers, local_costs, kind='cubic', fill_value='extrapolate')
+    return interp_func(np.arange(len(path)))
